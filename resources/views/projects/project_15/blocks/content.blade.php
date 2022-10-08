2660ff05551c421d69841c049176acb160268799
borashek <borashek@inbox.ru>
2022-10-08 17:43:16 +0300

    tts

--- resources/views/projects/project_15/blocks/content.blade.php
+++ resources/views/projects/project_15/blocks/content.blade.php
@@ -1,79 +1,89 @@
-<div class="main__content content">
-  <div class="content__left">
-      <div class="content__possibility possibility">
-          <div class="possibility__img-wrapper">
-              <img src="img/projects/project_15/possibilities/car.svg" alt="">
-          </div>
-          <div class="possibility__desc">
-              <h5 class="possibility__title">Подбирайте автомобиль</h5>
-              <p class="possibility__text">Более 10 000 новых автомобилей и с пробегом в наличии с подробной информацией</p>
-          </div>
+<div class="_container">
+    <div class="main__content content">
+        <div class="content__left">
+            <div class="content__possibility possibility">
+                <div class="possibility__img-wrapper">
+                    <img src="img/projects/project_15/possibilities/car.svg" class="possibility__small" alt="img">
+                    <img src="img/projects/project_15/content/car.png" class="possibility__big" alt="img">
+                </div>
+                <div class="possibility__desc">
+                    <h5 class="possibility__title">Подбирайте автомобиль</h5>
+                    <p class="possibility__text">Более 10 000 новых автомобилей и с пробегом в наличии с подробной информацией</p>
+                </div>
+            </div>
+            <div class="content__possibility possibility">
+                <div class="possibility__img-wrapper">
+                    <img src="img/projects/project_15/possibilities/wrench.svg" class="possibility__small" alt="wrench">
+                    <img src="img/projects/project_15/content/wrench.png" class="possibility__big" alt="img">
+                </div>
+                <div class="possibility__desc">
+                    <h5 class="possibility__title">Записывайтесь на сервис</h5>
+                    <p class="possibility__text">Быстрая на сервис на удобное время к Вашему мастеру-консультанту</p>
+                </div>
+            </div>
+            <div class="content__possibility possibility">
+                <div class="possibility__img-wrapper">
+                    <img src="img/projects/project_15/possibilities/star.svg" class="possibility__small" alt="img">
+                    <img src="img/projects/project_15/content/star.png" class="possibility__big" alt="img">
+                </div>
+                <div class="possibility__desc">
+                    <h5 class="possibility__title">Продавайте свой авто</h5>
+                    <p class="possibility__text">Оценка автомобиля по VIN или по параметрам, с заявкой на выкуп или трейд-ин</p>
+                </div>
+            </div>
+            <div class="content__possibility possibility">
+                <div class="possibility__img-wrapper">
+                    <img src="img/projects/project_15/possibilities/bill.svg" class="possibility__small" alt="img">
+                    <img src="img/projects/project_15/content/bill.png" class="possibility__big" alt="img">
+                </div>
+                <div class="possibility__desc">
+                    <h5 class="possibility__title">Оплачивайте счета</h5>
+                    <p class="possibility__text">Отслеживайте и оплачивайте счета за обслуживание прямо в приложении</p>
+                </div>
+            </div>
+            <div class="content__possibility possibility">
+                <div class="possibility__img-wrapper">
+                    <img src="img/projects/project_15/possibilities/history.svg" class="possibility__small" alt="img">
+                    <img src="img/projects/project_15/content/history.png" class="possibility__big" alt="img">
+                </div>
+                <div class="possibility__desc">
+                    <h5 class="possibility__title">История обслуживания</h5>
+                    <p class="possibility__text">История обслуживания вашего автомобиля всегда под рукой</p>
+                </div>
+            </div>
+            <div class="content__possibility possibility">
+                <div class="possibility__img-wrapper">
+                    <img src="img/projects/project_15/possibilities/chat.svg" class="possibility__small" alt="img">
+                    <img src="img/projects/project_15/content/chat.png" class="possibility__big" alt="img">
+                </div>
+                <div class="possibility__desc">
+                    <h5 class="possibility__title">Консультируйтесь в чате</h5>
+                    <p class="possibility__text">Простой способ решить любую ситуацию прямо в приложении</p>
+                </div>
+            </div>
+            <div class="content__possibility possibility">
+                <div class="possibility__img-wrapper">
+                    <img src="img/projects/project_15/possibilities/bonus.svg" class="possibility__small" alt="img">
+                    <img src="img/projects/project_15/content/bonus.png" class="possibility__big" alt="img">
+                </div>
+                <div class="possibility__desc">
+                    <h5 class="possibility__title">Бонусы всегда на виду</h5>
+                    <p class="possibility__text">Накопленные бонусные баллы отображаются  в приложении – используйте их при заезде в ТТС</p>
+                </div>
+            </div>
+            <div class="content__possibility possibility">
+                <div class="possibility__img-wrapper">
+                    <img src="img/projects/project_15/possibilities/store.svg" class="possibility__small" alt="store">
+                    <img src="img/projects/project_15/content/store.png" class="possibility__big" alt="img">
+                </div>
+                <div class="possibility__desc">
+                    <h5 class="possibility__title">Следите за акциями</h5>
+                    <p class="possibility__text">Самые свежие акции и персональные предложения, специально для Вас</p>
+                </div>
+            </div>
+        </div>
+        <div class="content__right">
+            <img src="img/projects/project_15/phone2.png" alt="phone">
+        </div>
       </div>
-      <div class="content__possibility possibility">
-          <div class="possibility__img-wrapper">
-              <img src="img/projects/project_15/possibilities/car.svg" alt="">
-          </div>
-          <div class="possibility__desc">
-              <h5 class="possibility__title">Записывайтесь на сервис</h5>
-              <p class="possibility__text">Быстрая на сервис на удобное время к Вашему мастеру-консультанту</p>
-          </div>
-      </div>
-      <div class="content__possibility possibility">
-          <div class="possibility__img-wrapper">
-              <img src="img/projects/project_15/possibilities/car.svg" alt="">
-          </div>
-          <div class="possibility__desc">
-              <h5 class="possibility__title">Продавайте свой авто</h5>
-              <p class="possibility__text">Оценка автомобиля по VIN или по параметрам, с заявкой на выкуп или трейд-ин</p>
-          </div>
-      </div>
-      <div class="content__possibility possibility">
-          <div class="possibility__img-wrapper">
-              <img src="img/projects/project_15/possibilities/car.svg" alt="">
-          </div>
-          <div class="possibility__desc">
-              <h5 class="possibility__title">Оплачивайте счета</h5>
-              <p class="possibility__text">Отслеживайте и оплачивайте счета за обслуживание прямо в приложении</p>
-          </div>
-      </div>
-      <div class="content__possibility possibility">
-          <div class="possibility__img-wrapper">
-              <img src="img/projects/project_15/possibilities/car.svg" alt="">
-          </div>
-          <div class="possibility__desc">
-              <h5 class="possibility__title">История обслуживания</h5>
-              <p class="possibility__text">История обслуживания вашего автомобиля всегда под рукой</p>
-          </div>
-      </div>
-      <div class="content__possibility possibility">
-          <div class="possibility__img-wrapper">
-              <img src="img/projects/project_15/possibilities/car.svg" alt="">
-          </div>
-          <div class="possibility__desc">
-              <h5 class="possibility__title">Консультируйтесь в чате</h5>
-              <p class="possibility__text">Простой способ решить любую ситуацию прямо в приложении</p>
-          </div>
-      </div>
-      <div class="content__possibility possibility">
-          <div class="possibility__img-wrapper">
-              <img src="img/projects/project_15/possibilities/car.svg" alt="">
-          </div>
-          <div class="possibility__desc">
-              <h5 class="possibility__title">Бонусы всегда на виду</h5>
-              <p class="possibility__text">Накопленные бонусные баллы отображаются  в приложении – используйте их при заезде в ТТС</p>
-          </div>
-      </div>
-      <div class="content__possibility possibility">
-          <div class="possibility__img-wrapper">
-              <img src="img/projects/project_15/possibilities/car.svg" alt="">
-          </div>
-          <div class="possibility__desc">
-              <h5 class="possibility__title">Следите за акциями</h5>
-              <p class="possibility__text">Самые свежие акции и персональные предложения, специально для Вас</p>
-          </div>
-      </div>
-  </div>
-  <div class="content__right">
-      <img src="img/projects/project_15/phone2.png" alt="phone">
-  </div>
 </div>
--- resources/views/projects/project_15/blocks/promo-bottom.blade.php
+++ resources/views/projects/project_15/blocks/promo-bottom.blade.php
@@ -1,19 +1,27 @@
-<div class="main__promo promo">
-  <div class="promo__left">
-      <h2 class="promo__title">Мой ТТС — Ваш умный и надежный автопомощник</h2>
-      <div class="promo__stores">
-          <div class="promo__store-wrapper">
-              <img src="img/projects/project_15/stores/app-store.jpg" alt="app-store">
-          </div>
-          <div class="promo__store-wrapper">
-              <img src="img/projects/project_15/stores/google-pay.jpg" alt="google-pay">
-          </div>
-          <div class="promo__store-wrapper">
-              <img src="img/projects/project_15/stores/huawei.jpg" alt="huawei">
-          </div>
-      </div>
-  </div>
-  <div class="promo__right">
-      <img src="img/projects/project_15/phone3.png" alt="phone">
-  </div>
+<div class="_container _container_promo">
+    <div class="main__promo promo promo_bottom">
+        <div class="promo__left">
+            <h2 class="promo__title">Скачивайте мобильное<br>приложение Мой ТТС </h2>
+            <div class="promo__stores">
+                <div class="promo__store-wrapper">
+                    <img src="img/projects/project_15/stores/app-store.jpg" alt="app-store">
+                </div>
+                <div class="promo__store-wrapper">
+                    <img src="img/projects/project_15/stores/google-pay.jpg" alt="google-pay">
+                </div>
+                <div class="promo__store-wrapper">
+                    <img src="img/projects/project_15/stores/huawei.jpg" alt="huawei">
+                </div>
+                <p>Доступно для IOS и Android</p>
+            </div>
+            <div class="promo__qr-wrapper">
+                <img src="img/projects/project_15/qr-code.jpg" alt="qr-code">
+                <p class="promo__qr-desc">Отсканируйте  QR-код<br>и установите приложение</p>
+                <h6>Установить</h6>
+            </div>
+        </div>
+    </div>
+    <div class="promo__right promo__right_bottom">
+        <img src="img/projects/project_15/phone3.png" alt="phone">
+    </div>
 </div>
